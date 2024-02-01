1842e73af98312fdde0f2d795f3e399753c7a625
adams miracle <adamsmiracle0@gmail.com>
2024-02-01 23:54:49 +0000

    task 1

--- 1-binary_tree_insert_left.c
+++ 1-binary_tree_insert_left.c
@@ -11,7 +11,7 @@ binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
 {
 	binary_tree_t *newNode;
 
-	newNode = malloc(sizeof(binary_tree_t));
+	newNode = binary_tree_node(parent, value);
 
 	if (newNode == NULL)
 	{
