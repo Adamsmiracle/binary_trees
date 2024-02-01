363d626623fba5955952e015baa68e01f439325f
adams miracle <adamsmiracle0@gmail.com>
2024-02-01 23:57:28 +0000

    task 1

--- 1-binary_tree_insert_left.c
+++ 1-binary_tree_insert_left.c
@@ -18,6 +18,11 @@ binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
 		return (NULL);
 	}
 
+	if (parent == NULL)
+	{
+		return (NULL);
+	}
+
 	if (parent->left != NULL)
 	{
 		newNode->left = parent->left;
