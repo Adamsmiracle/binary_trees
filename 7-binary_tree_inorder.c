b831d658b01b49a3cb38ce0f178b46a8126efa89
adams miracle <adamsmiracle0@gmail.com>
2024-02-02 09:43:16 +0000

    task 7

---
+++ 7-binary_tree_inorder.c
@@ -0,0 +1,18 @@
+#include "binary_trees.h"
+
+/**
+ * binary_tree_inorder - prints the binary tree in order
+ * @tree: the binary tree
+ * @func: function to act on the tree
+ * Return: Void
+ */
+
+void binary_tree_inorder(const binary_tree_t *tree, void (*func)(int))
+{
+	if (tree && funct)
+	{
+		binary_tree_inorder(tree->left, func);
+		funct(tree->n);
+		binary_tree_inorder(tree->right, func);
+	}
+}
