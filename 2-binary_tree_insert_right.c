bbc46544088a73af29b9d60421b418ebcc85466e
adams miracle <adamsmiracle0@gmail.com>
2024-02-02 00:55:50 +0000

    task 3

--- 2-binary_tree_insert_right.c
+++ 2-binary_tree_insert_right.c
@@ -13,6 +13,8 @@
  *              takes its place and the old right-child is set as
  *              the right-child of the new node.
  */
+
+
 binary_tree_t *binary_tree_insert_right(binary_tree_t *parent, int value)
 {
 	binary_tree_t *new;
@@ -21,6 +23,7 @@ binary_tree_t *binary_tree_insert_right(binary_tree_t *parent, int value)
 		return (NULL);
 
 	new = binary_tree_node(parent, value);
+	
 	if (new == NULL)
 		return (NULL);
 
